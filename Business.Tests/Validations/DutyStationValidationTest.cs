c8b366d98ff22de104dc1e73a664c33be4367a11
evgenii blazhevich <evgblazh2000@gmail.com>
2021-05-18 20:46:06 +0300

    Added unit tests  and some fix in Business Validation classes

---
+++ Business.Tests/Validations/DutyStationValidationTest.cs
@@ -0,0 +1,195 @@
+﻿using Moq;
+using NUnit.Framework;
+using PsuHistory.Business.Service.Interfaces;
+using PsuHistory.Business.Service.Validations;
+using PsuHistory.Data.Domain.Models.Monuments;
+using PsuHistory.Data.Service.Interfaces;
+using PsuHistory.Resource.Recources.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Tests.Validations
+{
+    class DutyStationValidationTest
+    {
+        private Mock<IBaseService<Guid, DutyStation>> _service;
+        private IBaseValidation<Guid, DutyStation> _validation;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new Mock<IBaseService<Guid, DutyStation>>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            //_dbContext.Dispose();
+        }
+
+        [Test]
+        public async Task GetValidationAsync_Succes()
+        {
+            // Arrange
+            await MockData(
+                dutyStation: new DutyStation()
+                );
+            var id = Guid.NewGuid();
+
+            // Act
+            var result = await _validation.GetValidationAsync(id);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.IsEmpty(result.Errors);
+                Assert.IsTrue(result.IsValid);
+            });
+        }
+
+        [Test]
+        public async Task GetValidationAsync_UnSucces()
+        {
+            // Arrange
+            await MockData();
+            var id = Guid.NewGuid();
+            var listError = new Dictionary<string, string>()
+            {
+                { nameof(DutyStation), BaseValidation.ObjectNotExistById }
+            };
+
+            // Act
+            var result = await _validation.GetValidationAsync(id);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.NotNull(result.Errors);
+                Assert.IsFalse(result.IsValid);
+                foreach (var error in result.Errors)
+                {
+                    Assert.IsTrue(listError.ContainsKey(error.Key));
+                    Assert.AreEqual(listError[error.Key], error.Value);
+                }
+            });
+        }
+
+        [Test]
+        public async Task InsertValidationAsync_Succes()
+        {
+            // Arrange
+            await MockData();
+            var entity = new DutyStation()
+            {
+                Place = "г. Полоцк"
+            };
+
+            // Act
+            var result = await _validation.InsertValidationAsync(entity);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.IsEmpty(result.Errors);
+                Assert.IsTrue(result.IsValid);
+            });
+        }
+
+        [Test]
+        public async Task UpdateValidationAsync_Succes()
+        {
+            // Arrange
+            await MockData(
+                dutyStation: new DutyStation()
+                );
+            var entity = new DutyStation()
+            {
+                Place = "г. Полоцк"
+            };
+
+            // Act
+            var result = await _validation.UpdateValidationAsync(entity);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.IsEmpty(result.Errors);
+                Assert.IsTrue(result.IsValid);
+            });
+        }
+
+        [Test]
+        public async Task DeleteValidationAsync_Succes()
+        {
+            // Arrange
+            await MockData(
+                dutyStation: new DutyStation()
+                );
+            var id = Guid.NewGuid();
+
+            // Act
+            var result = await _validation.DeleteValidationAsync(id);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.IsEmpty(result.Errors);
+                Assert.IsTrue(result.IsValid);
+            });
+        }
+
+        [Test]
+        public async Task DeleteValidationAsync_UnSucces()
+        {
+            // Arrange
+            await MockData();
+            var id = Guid.NewGuid();
+            var listError = new Dictionary<string, string>()
+            {
+                { nameof(DutyStation), BaseValidation.ObjectNotExistById }
+            };
+
+            // Act
+            var result = await _validation.DeleteValidationAsync(id);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.NotNull(result.Errors);
+                Assert.IsFalse(result.IsValid);
+                foreach (var error in result.Errors)
+                {
+                    Assert.IsTrue(listError.ContainsKey(error.Key));
+                    Assert.AreEqual(listError[error.Key], error.Value);
+                }
+            });
+        }
+
+        private async Task MockData(
+            DutyStation dutyStation = null,
+            bool isExist = false
+            )
+        {
+            _service.Setup(x => x.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).ReturnsAsync(dutyStation);
+            _service.Setup(x => x.ExistAsync(It.IsAny<DutyStation>(), It.IsAny<CancellationToken>())).ReturnsAsync(isExist);
+
+            _validation = new DutyStationValidation(_service.Object);
+        }
+
+        private string GetBaseValidationResources(string name)
+        {
+            switch (name)
+            {
+                case "ObjectExistWithThisData": return BaseValidation.ObjectExistWithThisData; break;
+                case "FieldNotCanBeNull": return BaseValidation.FieldNotCanBeNull; break;
+                case "FieldInvalidLength": return BaseValidation.FieldInvalidLength; break;
+                case "ObjectNotExistById": return BaseValidation.ObjectNotExistById; break;
+            }
+            return null;
+        }
+    }
+}
