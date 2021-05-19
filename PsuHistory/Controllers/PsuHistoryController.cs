bb683e2f2430176ca333c9e5a177b94d02f5b77b
evgenii blazhevich <evgblazh2000@gmail.com>
2021-05-19 21:18:17 +0300

    Some fix for controllers

---
+++ PsuHistory/Controllers/PsuHistoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PsuHistory.Controllers.Abstraction;
+
+namespace PsuHistory.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class PsuHistoryController : AbstractionControllerBase
+    {
+    }
+}
