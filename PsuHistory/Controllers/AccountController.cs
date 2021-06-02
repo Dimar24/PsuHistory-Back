5d27bb26bc7c0a28f9f4de8df93bb90f25b9ffee
Дмитрий Маркавец <markavets.do@gazgolder.ru>
2021-06-02 11:51:26 +0300

    small fix

--- PsuHistory/Controllers/AccountController.cs
+++ PsuHistory/Controllers/AccountController.cs
@@ -1,17 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using PsuHistory.Controllers.Abstraction;
 using PsuHistory.Business.DTO.Models.AccountDataModels;
 using PsuHistory.Business.Service.Interfaces;
-using PsuHistory.Common.Models;
-using PsuHistory.Data.Domain.Models.Users;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
+using PsuHistory.Controllers.Abstraction;
 using System.Threading.Tasks;
 
 namespace PsuHistory.Controllers
@@ -30,14 +21,6 @@ namespace PsuHistory.Controllers
             this.mapper = mapper;
             this.accountService = accountService;
         }
-
-        // тестовые данные вместо использования базы данных
-        private List<User> users = new List<User>
-        {
-            new User { Mail = "Admin", Password = "secret", Role = new Role() { Name = "Admin" } },
-            new User { Mail = "Moderator", Password = "secret", Role = new Role() { Name = "Moderator" } },
-            new User { Mail = "user", Password = "secret", Role = new Role() { Name = "user" } }
-        };
 
         [Route("login")]
         [HttpPost]
